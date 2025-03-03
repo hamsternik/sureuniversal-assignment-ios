0df9870db19b9c85752817ee7e687cbc3ee49204
Nikita Khomitsevych <hamsternik9@gmail.com>
2025-03-03 18:12:15 +0000

    Initial Commit

---
+++ SureUniversalAssignment/SureUniversalAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SureUniversalAssignmentApp.swift
+//  SureUniversalAssignment
+//
+//  Created by Niki Khomitsevych on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SureUniversalAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
