1b83d7bd6ae5dc4bbee40a732e81662660aa1dca
Nikita Khomitsevych <hamsternik9@gmail.com>
2025-03-05 13:27:33 +0000

    chore: pull out UserView

--- Sources/UsersRootView.swift
+++ Sources/UsersRootView.swift
@@ -64,7 +64,7 @@ struct UsersRootView: View {
     }
 }
 
-// MARK: Preview (UsersRootView)
+// MARK: Preview 
 
 struct UsersViewPreview: PreviewProvider {
     static var previews: some View {
@@ -80,66 +80,5 @@ struct UsersViewPreview: PreviewProvider {
             ),
             usersController: UsersController(apiClient: ApiClient())
         )
-    }
-}
-
-// MARK: - UserView
-
-struct UserView: View {
-    struct Props: Hashable, Identifiable {
-        let id: Int
-        let name: String
-    }
-    
-    let props: Props
-    init(props: Props) {
-        self.props = props
-    }
-    
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack(spacing: 4) {
-                Text("User \(props.id)")
-                    .font(.system(size: 14, weight: .medium, design: .default))
-                Text("\(props.name)")
-                    .font(.system(size: 14, weight: .regular, design: .default))
-            }
-            .padding(.vertical, 40)
-            Spacer()
-        }
-        .background(
-            RoundedRectangle(cornerRadius: 8)
-                .fill(Color.Users.rootBackground)
-                .stroke(Color.User.stroke, lineWidth: 1)
-        )
-        .padding(.horizontal, 16)
-    }
-}
-
-// MARK: Preview (UserView)
-
-struct UserView_Preview: PreviewProvider {
-    static var previews: some View {
-        VStack {
-            UserView(
-                props: .init(
-                    id: 1,
-                    name: "Leanne Graham"
-                )
-            )
-            UserView(
-                props: .init(
-                    id: 2, 
-                    name: "Ervin Howell"
-                )
-            )
-            UserView(
-                props: .init(
-                    id: 3,
-                    name: "Clementine Bauch"
-                )
-            )
-        }
     }
 }
